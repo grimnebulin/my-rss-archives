525eef663f39ba378d832950356b88cdcbba322c
Sean McAfee <eefacm@gmail.com>
2013-09-20 15:38:20 -0700

    Increases ITEMS_TO_FETCH.

--- MegaTokyoArchive.pm
+++ MegaTokyoArchive.pm
@@ -7,7 +7,8 @@ use constant {
     FEED_TITLE     => 'MegaTokyo Archive',
     RSS_FILE       => "$ENV{HOME}/www/rss/megatokyo.xml",
     FIRST_PAGE     => 'http://megatokyo.com/strip/1',
-    ITEMS_TO_FETCH => 3,
+    ITEMS_TO_FETCH => 5,
+    ITEMS_TO_KEEP  => 20,
     RENDER         => '//span[@id="strip"]//img[contains(@src,"strips/")]',
     NEXT_PAGE      => '//li[contains(concat(" ",normalize-space(@class)," ")," next ")]//a/@href',
 };
--- SluggyArchive.pm
+++ SluggyArchive.pm
@@ -7,7 +7,7 @@ use constant {
     FEED_TITLE     => 'Sluggy Freelance Archive',
     RSS_FILE       => "$ENV{HOME}/www/rss/sluggy.xml",
     FIRST_PAGE     => 'http://www.sluggy.com/comics/archives/daily/970825',
-    ITEMS_TO_FETCH => 3,
+    ITEMS_TO_FETCH => 5,
     RENDER         => '//div[contains(@class,"comic_content")]//img[contains(@src,"/images/comics/")]',
     NEXT_PAGE      => '//div[@id="comic_navigation"]//a[normalize-space()="Next"]/@href',
 };
