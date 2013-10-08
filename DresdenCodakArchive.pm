c17cda4bb44bb36a9f1c99b7e321fb806d713b48
Sean McAfee <eefacm@gmail.com>
2013-10-08 14:59:55 -0700

    Reduces to one article per day.

--- DresdenCodakArchive.pm
+++ DresdenCodakArchive.pm
@@ -7,7 +7,6 @@ use constant {
     FEED_TITLE     => 'Dresden Codak Archive',
     RSS_FILE       => "$ENV{HOME}/www/rss/dresden.xml",
     FIRST_PAGE     => 'http://dresdencodak.com/2005/06/08/the-tomorrow-man/',
-    ITEMS_TO_FETCH => 3,
     RENDER         => '//div[@id="comic"]//img',
     NEXT_PAGE      => '//div[contains(@class,"menunav")]//a[img[contains(@src,"m_next")]]/@href',
 };
