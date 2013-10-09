193bf4130cf9b52e36ac097dacf7941b444a1dcb
Sean McAfee <eefacm@gmail.com>
2013-10-09 13:09:42 -0700

    Increases count of items fetched.

--- MonkeyBusinessArchive.pm
+++ MonkeyBusinessArchive.pm
@@ -7,7 +7,7 @@ use constant {
     FEED_TITLE     => 'Monkey Business Archive',
     RSS_FILE       => "$ENV{HOME}/www/rss/monkeybusiness.xml",
     FIRST_PAGE     => 'http://monkey-business.net/2003/08/25/nice-try-punk/',
-    ITEMS_TO_FETCH => 2,
+    ITEMS_TO_FETCH => 3,
     RENDER         => '//div[contains(@class,"comicpane")]//img',
     NEXT_PAGE      => '//a[@id="comic-nav-next"]/@href',
 };
