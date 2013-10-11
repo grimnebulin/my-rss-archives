a6e328ee360696aea974087d305a1cb1675b2ebb
Sean McAfee <eefacm@gmail.com>
2013-10-11 15:46:41 -0700

    Adds custom title.

--- SinfestArchive.pm
+++ SinfestArchive.pm
@@ -12,4 +12,16 @@ use constant {
     NEXT_PAGE      => '//a[img[@alt="Next"]]/@href',
 };
 
+
+sub title {
+    my ($self, $tree, $uri) = @_;
+    for my $text (map $_->getValue, $tree->findnodes('//text()')) {
+        if ($text =~ /^2[01]\d{2}-\d{2}-\d{2}:/) {
+            return $text;
+        }
+    }
+    return $self->SUPER::title($tree, $uri);
+}
+
+
 1;
