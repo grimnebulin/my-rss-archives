f4392bccf3e961292d1358b210ebed8996577436
Sean McAfee <eefacm@gmail.com>
2013-10-13 12:17:52 -0700

    Fixes NEXT_PAGE XPath.

--- ScalaNeophyteArchive.pm
+++ ScalaNeophyteArchive.pm
@@ -8,7 +8,7 @@ use constant {
     RSS_FILE   => "$ENV{HOME}/www/rss/scalaneo.xml",
     FIRST_PAGE => 'http://danielwestheide.com/blog/2012/11/21/the-neophytes-guide-to-scala-part-1-extractors.html',
     RENDER     => '//div[contains(@class,"entry-content")]',
-    NEXT_PAGE  => '//a[starts-with(@title,"Next post")]/@href',
+    NEXT_PAGE  => '//a[starts-with(@title,"Next Post")]/@href',
 };
 
 1;
