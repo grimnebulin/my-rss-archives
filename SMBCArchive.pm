b94a150b6b73feb79a9dc6b3a88b73004307828b
Sean McAfee <eefacm@gmail.com>
2013-12-17 15:11:56 -0500

    Scale back comics-per-day a bit; incorporate date into title.

--- SMBCArchive.pm
+++ SMBCArchive.pm
@@ -6,10 +6,23 @@ use strict;
 use constant {
     FEED_TITLE     => 'Saturday Morning Breakfast Cereal Archive',
     RSS_FILE       => "$ENV{HOME}/www/rss/smbc.xml",
-    ITEMS_TO_FETCH => 5,
+    ITEMS_TO_FETCH => 3,
     FIRST_PAGE     => 'http://www.smbc-comics.com/?id=1#comic',
     RENDER         => '//div[@id="comicimage"]//img',
     NEXT_PAGE      => '//a[contains(@class,"nextRollover")]/@href',
 };
+
+
+sub title {
+    my ($self, $doc) = @_;
+    my $title = $self->SUPER::title($doc);
+    if (my ($img) = $self->render($doc)) {
+        if ($img->attr('src') =~ m|/comics/(\d{4})(\d{2})(\d{2})|) {
+            $title .= " - $2/$3/$1";
+        }
+    }
+    return $title;
+}
+
 
 1;
