242e6192298be55ed973b37326fec0696ec1863d
Sean McAfee <eefacm@gmail.com>
2014-01-04 23:37:34 -0800

    Removes unnecessary elements from rendered form.
    Also, uses newer find() routine rather than findnodes().

--- PrincessPlanetArchive.pm
+++ PrincessPlanetArchive.pm
@@ -8,18 +8,28 @@ use constant {
     RSS_FILE       => "$ENV{HOME}/www/rss/princessplanet.xml",
     ITEMS_TO_FETCH => 3,
     FIRST_PAGE     => 'http://www.theprincessplanet.com/2004/11/13/welcome-to-the-princess-planet/',
-    RENDER         => '//div[@id="comic"]/img',
-    NEXT_PAGE      => '//div[contains(@class,"nav")]/a[@rel="next"]/@href',
+    NEXT_PAGE      => [ '//div[%s]/a[@rel="next"]/@href', 'nav' ],
 };
 
 
 sub title {
     my ($self, $doc) = @_;
     my $title = $self->SUPER::title($doc);
-    if (my ($date) = $doc->findnodes('//div[contains(@class,"comicdate")]')) {
+    if (my ($date) = $doc->find('//div[%s]', 'comicdate')) {
         $title .= ' - ' . $date->as_trimmed_text;
     }
     return $title;
+}
+
+sub render {
+    my ($self, $doc) = @_;
+    my ($img) = $doc->findnodes('//div[@id="comic"]/img') or return;
+    my @comment = $doc->findnodes(
+        '//div[%s]/*[not(self::script)]' .
+        '[not(@*[starts-with(local-name(),"data-")])]',
+        'entry'
+    );
+    return ($self->new_element('div', $img), @comment);
 }
 
 
