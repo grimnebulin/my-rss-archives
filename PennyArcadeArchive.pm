7c2acf8cc1bafa2822e97f94e0877bc67b4b4d6f
Sean McAfee <eefacm@gmail.com>
2014-07-02 22:34:16 -0700

    Fix rendering logic.

--- PennyArcadeArchive.pm
+++ PennyArcadeArchive.pm
@@ -8,7 +8,7 @@ use constant {
     RSS_FILE       => "$ENV{HOME}/www/rss/pennyarcade.xml",
     FIRST_PAGE     => 'http://www.penny-arcade.com/comic/1998/11/18',
     ITEMS_TO_FETCH => 5,
-    RENDER         => [ '//div[%s]/img', 'comic' ],
+    RENDER         => '//div[@id="comicFrame"]//img',
     NEXT_PAGE      => '//a[@title="Next"]/@href',
 };
 
