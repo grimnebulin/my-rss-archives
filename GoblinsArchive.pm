370ebe3bb65ed13770b6ae1ccdbf321e5211b77c
Sean McAfee <eefacm@gmail.com>
2014-07-02 22:33:49 -0700

    Fetch five items instead of two.

--- GoblinsArchive.pm
+++ GoblinsArchive.pm
@@ -7,7 +7,7 @@ use constant {
     FEED_TITLE     => 'Goblins Archive',
     RSS_FILE       => "$ENV{HOME}/www/rss/goblins.xml",
     FIRST_PAGE     => 'http://www.goblinscomic.org/06262005/',
-    ITEMS_TO_FETCH => 2,
+    ITEMS_TO_FETCH => 5,
     RENDER         => '//div[@id="comic"]//img',
     NEXT_PAGE      => '//div[@class="nav-next"]/a[@rel="next"]/@href',
 };
