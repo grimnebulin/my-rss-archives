78cb321bf4f006714c22a1bd7415fecffecb1337
Sean McAfee <eefacm@gmail.com>
2014-07-02 22:35:32 -0700

    Append comic-number-of-the-day to titles.

--- QCArchive.pm
+++ QCArchive.pm
@@ -12,14 +12,30 @@ use constant {
     NEXT_PAGE      => '//a[normalize-space()="Next"]/@href',
 };
 
+my $count = 0;
+
+
+sub title {
+    my ($self, $doc) = @_;
+    return sprintf '%s (#%d)', scalar $self->SUPER::title($doc), ++$count;
+}
 
 sub render {
     my ($self, $doc) = @_;
-    my ($image) = $doc->findnodes('//div[@id="comic"]//img[contains(@src,"/comics/")]')
-        or return;
+
+    my ($image) = $doc->findnodes(
+        '//div[@id="comic"]//img[contains(@src,"/comics/")]'
+    ) or return;
+
+    $image = $self->new_element(
+        'div', { style => 'margin-top: 65px' }, $image
+    );
+
     my ($news) = $doc->findnodes('//div[@id="news"]');
     $news = "" if $news && $news->as_trimmed_text =~ /no news today, sorry/i;
+
     return ($image, $news);
+
 }
 
 
