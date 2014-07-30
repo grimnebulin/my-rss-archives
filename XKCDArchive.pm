6af1f5fa8fc0bdb7bf1b63547011bf68efb3fe20
Sean McAfee <eefacm@gmail.com>
2014-07-30 12:49:37 -0700

    Consults xkcd site to obtain strip dates.

--- XKCDArchive.pm
+++ XKCDArchive.pm
@@ -11,6 +11,8 @@ use constant {
     NEXT_PAGE      => [ '//ul[%s]//a[@rel="next"]/@href', 'comicNav' ],
 };
 
+my $TOC_URL = 'http://xkcd.com/archive/';
+
 sub render {
     my ($self, $doc) = @_;
     my ($img) = $doc->find('//div[@id="comic"]/img') or return;
@@ -18,6 +20,28 @@ sub render {
     return (
         $img, $title && $self->new_element('p', [ 'i', $title ])
     );
+}
+
+sub table_of_contents {
+    my $self = shift;
+    return $self->{toc} ||= do {
+        my $toc = $self->get_doc($TOC_URL);
+        return {
+            map {
+                $_->attr('title') =~ /^(\d+)-(\d+)-(\d+)\z/
+                    ? ($toc->resolve($_->attr('href')), "$2-$3-$1")
+                    : ();
+            } $toc->find('//a')
+        };
+    };
+}
+
+sub title {
+    my ($self, $doc) = @_;
+    my $title = $self->SUPER::title($doc);
+    my $date = $self->table_of_contents->{ $doc->source };
+    $title .= ' - ' . $date if defined $date;
+    return $title;
 }
 
 1;
