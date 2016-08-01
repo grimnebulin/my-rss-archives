a58d50e1ddef7da772b6046bd12447b4d2d06bba
Sean McAfee <eefacm@gmail.com>
2016-07-31 17:34:52 -0700

    KODT tweaks

--- KODTArchive.pm
+++ KODTArchive.pm
@@ -7,18 +7,25 @@ use constant {
     FEED_TITLE     => 'KODT Archive',
     RSS_FILE       => "$ENV{HOME}/www/rss/kodt.xml",
     FIRST_PAGE     => 'http://www.kodtweb.com/2011/05/16/hello-world/',
-    ITEMS_TO_FETCH => 5,
     NEXT_PAGE      => [ '//td[%s]/a[%s]/@href', 'comic_navi_right', 'navi-next' ],
+    END_OF_ARCHIVE_NOTIFY => 0,
 };
 
 
 sub render {
     my ($self, $doc) = @_;
-    my @img = $doc->find('//div[%s]/img', 'comicpane') or return;
+    my @img = $doc->find('//div[%s]//img', 'comicpane') or return;
     my ($entry) = $doc->find('//div[%s]//div[%s]', 'post', 'entry');
     return (@img, $entry);
 }
 
+sub title {
+    my ($self, $doc) = @_;
+    my $title = $self->SUPER::title($doc);
+    my ($date) = $doc->find('//span[%s]', 'post-date');
+    $title .= ' - ' . $date->as_trimmed_text if defined $date;
+    return $title;
+}
+
 
 1;
-
