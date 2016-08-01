51ddc2d7cbd8e82da6e433df2d2ca5b38753f575
Sean McAfee <eefacm@gmail.com>
2016-07-31 17:35:09 -0700

    Sinfest tweaks

--- SinfestArchive.pm
+++ SinfestArchive.pm
@@ -6,17 +6,19 @@ use strict;
 use constant {
     FEED_TITLE     => 'Sinfest Archive',
     RSS_FILE       => "$ENV{HOME}/www/rss/sinfest.xml",
-    FIRST_PAGE     => 'http://www.sinfest.net/archive_page.php?comicID=1',
+    FIRST_PAGE     => 'http://sinfest.net/view.php?date=2000-01-17',
     ITEMS_TO_FETCH => 5,
-    RENDER         => '//p[@align="center"][count(*)=1]/img',
-    NEXT_PAGE      => '//a[img[@alt="Next"]]/@href',
+    RENDER         => '//img[contains(@src,"/comics/")]',
+    NEXT_PAGE      => '//a[img[contains(@src,"/next.gif")]]/@href',
+    AGENT_ID       => 'Anything',
 };
 
 
 sub title {
     my ($self, $doc) = @_;
-    for my $text (map $_->getValue, $doc->findnodes('//text()')) {
-        if ($text =~ /^2[01]\d{2}-\d{2}-\d{2}:/) {
+    for my $td ($doc->findnodes('//td')) {
+        my $text = $td->as_trimmed_text;
+        if ($text =~ /^(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)/) {
             return $text;
         }
     }
