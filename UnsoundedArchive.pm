b9a865ede0ab2b6743f4aa50390a1cff17abb348
Sean McAfee <eefacm@gmail.com>
2017-01-15 11:20:29 -0800

    Fix Unsounded next-page logic

--- UnsoundedArchive.pm
+++ UnsoundedArchive.pm
@@ -8,7 +8,7 @@ use constant {
     RSS_FILE   => "$ENV{HOME}/www/rss/unsounded.xml",
     FIRST_PAGE => 'http://www.casualvillain.com/Unsounded/comic/ch01/ch01_01.html',
     RENDER     => '//div[@id="comic"]//img',
-    NEXT_PAGE  => [ '//div[@id="footer"]//a[%s]/@href', 'forward' ],
+    NEXT_PAGE  => '//div[@id="footer"]//a[@title="forward"]/@href',
 };
 
 
